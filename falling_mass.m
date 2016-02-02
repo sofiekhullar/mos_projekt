2a448c3a14beadc974982d545a501496a6bba6d4
sofiekhullar <sofie_k@live.se>
2016-02-02 11:36:22 +0100

    Randomfunk

--- falling_mass.m
+++ falling_mass.m
@@ -1,18 +1,20 @@
 clear all;
 V0 = 0; % initial speed
-m = 0.05; % mass in kg
+m = 0.005; % mass in kg
 g = 9.81; % gravity acceleration kg/m3
 rho = 1.2; % Air density
-A = 0.009; % Object area
+A = 4.4e-3; % Object area
 cw = 0.4; % Numerical drag coefficient
 k = 0.5*cw*rho*A; % Coefficient
 N = 100; % Time step
 V = zeros(1,N); % Speed
 V(1)=V0;
 deltat=0.2;
-pos(1) = 1000; % Start height
+pos(1) = 10; % Start height
 
-t=(0:N-1)*deltat;
+t=(0:N-1)*deltat; %Tiden
+
+R = 0.1e-3+4.41e-3*rand(1,1) % Random nr mellan max och min
 
 for i=1:N-1
 V(i+1)=V(i)+deltat*(g-(k/m)*V(i)^2); %c calculate the velocity
