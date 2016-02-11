2063f73595148a0c46bd7f4e19bf8a9728797d39
sofiekhullar <sofie_k@live.se>
2016-02-11 13:40:29 +0100

    struktur och const

--- webGL/object.js
+++ webGL/object.js
@@ -1,48 +1,34 @@
 			
+			// creating the scene
 			var scene = new THREE.Scene();
 			var camera = new THREE.PerspectiveCamera( 100, window.innerWidth/window.innerHeight, 0.1, 1000 );
-			var clock = new THREE.Clock();
-    		var time = 0;
-    		var fallingHor = true;
-
-    		var N = 100;
-			var posY = calculate();  
-			var t = createTime(N);   // FULKOD?!?!?
-			var counter = 0;
-
+			
 			var renderer = new THREE.WebGLRenderer();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
-			
-			var geometry = new THREE.BoxGeometry( 20, 1, 20 );
-			var material = new THREE.MeshPhongMaterial( { color: 0x545454, specular: 0xffffff, shininess: 100, shading: THREE.FlatShading } );
-			var cube = new THREE.Mesh( geometry, material );
-			
-			cube.position.x = 0;
-			cube.position.y = 100;
-			//cube.position.z = 50;
-
-
-			if(fallingHor){
-				cube.rotation.x = Math.PI /2;
-			}
-			scene.add( cube );
-
-			//camera.position.z = 5;
 
 			camera.position.x = 0;
        		camera.position.y = 0;
        		camera.position.z = 100;
 
-			//Gör scenen
-
-			//Lägga till ljus
+       		// add light
 			var light = new THREE.SpotLight(0xFFFFFF, 5); //Vitt ljus och intensitet (jättestarkt!).
 	      	light.position.set( 50, 100, 50 );
 	      	scene.add(light);
 	      	light.castShadow = true;
 
-			//Lägg till golv
+			// variabler
+			var clock = new THREE.Clock();
+    		var time = 0;
+    		var fallingHor = true;
+    		var N = 100;
+			var posY = calculate();  
+			var t = createTime(N);   // FULKOD?!?!?
+			var counter = 0;
+			var glitter = [];
+			var max_of_glitter = 5;
+
+			// add floor
 	      	var planeGeo = new THREE.PlaneGeometry(100, 100, 10, 10);
 			var planeMat = new THREE.MeshLambertMaterial({color: 0xFFFFFF});
 			var plane = new THREE.Mesh(planeGeo, planeMat);		
@@ -51,12 +37,35 @@
 			plane.position.y = -50;
 
 			//Add wireframe for box  
-			var wire = new THREE.Mesh(new THREE.CubeGeometry(100, 100, 100), new THREE.MeshBasicMaterial({wireframe: true, color: 'blue'}));      		
+			var wire = new THREE.Mesh(new THREE.CubeGeometry(100, 100, 100), new THREE.MeshBasicMaterial({wireframe: true, color: 'white'}));      		
       		scene.add(wire);
 			plane.receiveShadow = true;
 			scene.add(plane);
 
+			// Creating the box
+			var geometry = new THREE.BoxGeometry( 20, 1, 20 );
+			var material = new THREE.MeshPhongMaterial( { color: 0x545454, specular: 0xffffff, shininess: 100, shading: THREE.FlatShading } );
+			var cube = new THREE.Mesh( geometry, material );
+			
+			cube.position.x = 0;
+			cube.position.y = 100;
+			//cube.position.z = 50;
 
+			//Create boxes and store in array
+ 			var geo = new THREE.BoxGeometry( 1, 0.1, 1 );
+ 			   for (var i = 0; i < max_of_glitter; i++) {
+ 				        var box = {};
+ 				        var mat = new THREE.MeshBasicMaterial({color: Math.floor(Math.random() * 0x1000000)});
+ 				        box.obj = new THREE.Mesh( geo, mat);
+ 				    };
+ 			box.x = 0;
+			box.y = 100;
+
+			// Check fallingway
+			if(fallingHor){
+				cube.rotation.x = Math.PI /2;
+			}
+			scene.add( cube );
 
 			
 			var render = function () {
@@ -83,15 +92,15 @@
 
 	function calculate () {
 
-		var V0 = 0; // initial speed
-		var m = 0.00005; // mass in kg
-		var g = 9.82; // gravity acceleration kg/m3
-		var rho = 1.2; // Air density
-		var Amax = 0.0044; // Object maxarea
-		var Amin = 0.0001; // Object minarea
-		var cw = 0.4; // Numerical drag coefficient
-		var deltat=0.2;
-		var N = 200; // Time step
+		const V0 = 0; // initial speed
+		const m = 0.00005; // mass in kg
+		const g = 9.82; // gravity acceleration kg/m3
+		const rho = 1.2; // Air density
+		const Amax = 0.0044; // Object maxarea
+		const Amin = 0.0001; // Object minarea
+		const cw = 0.4; // Numerical drag coefficient
+		const deltat=0.2;
+		const N = 200; // Time step
 		var V = new Array(N); // Speed
 		var posY = new Array(N); //Height
 		V[0]=V0; //Start velocity
